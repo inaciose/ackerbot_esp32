865f247ebb63143160e37c941277756a68f23637
inaciose <inaciose@gmail.com>
2021-08-16 22:03:51 +0100

    OLED screen added

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,37 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-const char* ssid = "";
-const char* password = "";
+#include "wifi.h"
 
 #define LED_BUILTIN 2
 #define LED_TIMER 500
+
+//#define ENABLE_MPU6050 //if want use oled,turn on this macro
+#define ENABLE_OLED //if want use oled,turn on this macro
+
+#if defined(ENABLE_OLED) || defined(ENABLE_MPU6050)
+  #include <Wire.h>
+#endif
+
+#ifdef ENABLE_OLED
+// oled
+#include "SSD1306Wire.h"
+#define OLED_ADDRESS 0x3c
+#define I2C_SDA 14
+#define I2C_SCL 15
+SSD1306Wire display(OLED_ADDRESS, SDA, SCL, GEOMETRY_128_64);
+bool hasDisplay = false; // we probe for the device at runtime
+#endif
+
+void lcdMessage(String msg) {
+  #ifdef ENABLE_OLED
+    if(hasDisplay) {
+        display.clear();
+        display.drawString(128 / 2, 32 / 2, msg);
+        display.display();
+    }
+  #endif
+}
 
 void blinkLedBuiltin() {
 	static boolean ledstate = 0;
@@ -22,16 +48,59 @@ void blinkLedBuiltin() {
 }
 
 void setup() {
+
+  // WIFI mode
+  int softap_mode = 0;
+  // client mode ip
+  IPAddress ip;
+  // access point mode ip
+  IPAddress apIP = IPAddress(192, 168, 4, 1);
+
+  // led config
   pinMode(LED_BUILTIN, OUTPUT);
 
+  // serial config
   Serial.begin(115200);
   Serial.println("Booting");
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+
+  #ifdef ENABLE_OLED
+    hasDisplay = display.init();
+    if(hasDisplay) {
+        display.flipScreenVertically();
+        display.setFont(ArialMT_Plain_16);
+        display.setTextAlignment(TEXT_ALIGN_CENTER);
+        display.setContrast(255);
+    }  
+    lcdMessage("booting");
+  #endif
+
+  // WIFI
+  if(softap_mode) {
+    const char *hostname = "akbesp";
+    lcdMessage("starting softAP");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    bool result = WiFi.softAP(hostname, "12345678", 1, 0);
+    if (!result) {
+      Serial.println("AP Config failed.");
+      return;
+    } else {
+      Serial.println("AP Config Success.");
+      Serial.print("AP MAC: ");
+      Serial.println(WiFi.softAPmacAddress());
+      ip = WiFi.softAPIP();
+    }
+  } else {
+    lcdMessage(String("join ") + ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(F("."));
+    }
+    ip = WiFi.localIP();
+    Serial.println(F("WiFi connected"));
   }
 
   // Port defaults to 3232
@@ -57,12 +126,32 @@ void setup() {
 
       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
       Serial.println("Start updating " + type);
+
+      #ifdef ENABLE_OLED
+        display.clear();
+        display.setFont(ArialMT_Plain_10);
+        display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+        display.drawString(display.getWidth() / 2, display.getHeight() / 2 - 10, "OTA Update");
+        display.display();
+      #endif
+
     })
     .onEnd([]() {
       Serial.println("\nEnd");
+      #ifdef ENABLE_OLED
+        display.clear();
+        display.setFont(ArialMT_Plain_10);
+        display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "OTA done: Restart");
+        display.display();
+      #endif
     })
     .onProgress([](unsigned int progress, unsigned int total) {
       Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      #ifdef ENABLE_OLED
+        display.drawProgressBar(4, 32, 120, 8, progress / (total / 100) );
+        display.display();
+      #endif
     })
     .onError([](ota_error_t error) {
       Serial.printf("Error[%u]: ", error);
@@ -71,13 +160,24 @@ void setup() {
       else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
       else if (error == OTA_END_ERROR) Serial.println("End Failed");
+      #ifdef ENABLE_OLED
+        if (error == OTA_AUTH_ERROR) lcdMessage("OTA error: Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) lcdMessage("OTA error: Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) lcdMessage("OTA error: Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) lcdMessage("OTA error: Receive Failed");
+        else if (error == OTA_END_ERROR) lcdMessage("OTA error: End Failed");
+        else lcdMessage("OTA error: other");
+      #endif
+
     });
 
   ArduinoOTA.begin();
 
   Serial.println("Ready");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(ip);
+  lcdMessage(ip.toString());
+
 }
 
 void loop() {
